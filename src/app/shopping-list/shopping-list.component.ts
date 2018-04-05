3a9040ea9d927613d1973b5ac146b8b79dc0440d
zac-zaoxuanzhou <zaczzx@163.com>
2018-04-05 16:50:16 -0500

    use Event, ngIf, @Output() to change navi

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -9,7 +9,7 @@ import { Ingredient } from '../shared/ingredient.model';
 })
 export class ShoppingListComponent implements OnInit {
   ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5);
+    new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10);
   ]
 
