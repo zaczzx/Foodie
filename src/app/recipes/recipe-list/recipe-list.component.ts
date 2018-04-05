bf4d34cfd9df9a5c9b8d7f63c4fcbde47233a60c
zac-zaoxuanzhou <zaczzx@163.com>
2018-04-05 14:03:54 -0500

    refactor recipe-list recipe-detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [new Recipe('TEST', 'THIS IS FOR TEST',
+    'https://cdn3.tmbi.com/secure/RMS/attachments/37/300x300/exps25157_FM153592B03_18_12b.jpg')
+  ];
+
 
   constructor() { }
 
